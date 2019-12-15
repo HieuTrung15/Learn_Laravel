e4f29da8fffd083b70a317fb040f97448002518f
HieuTrung15 <hieutrungvietnam0107@gmail.com>
2019-12-15 21:24:55 +0700

    done UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <title>{{config('app.name', 'HT5')}}</title>
 </head>
 <body>
-    @yield('content')
+    @include('inc.navbar')
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 @section('content')
 {{-- section dung de thay doi noi dung ben trong content --}}
-<h1>This is Index Pages</h1>
-<h2>{{$title}}</h2>
+    <div class="jumpbotron text-center">
+        <h1>Welcome to laravel</h1>
+        <p>This is the laravel application from the "Laravel by Brad"</p>
+        <p><a href="/login" class="btn btn-primary btn-lg" role="button" >Login</a> <a href="/resgiter" class="btn btn-success btn-lg" role="button">Resgiter</a></p>
+    </div>
 @endsection
