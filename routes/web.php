786357d69de856d7dd6da5d4fdf8747a3147428a
HieuTrung15 <hieutrungvietnam0107@gmail.com>
2019-12-22 17:05:04 +0700

    logout to index page

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.index');
 });
 Route::get('/index', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
@@ -28,4 +28,4 @@ Route::post('/destroy/{id}', 'PostsController@destroy');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'DashboardController@index');
