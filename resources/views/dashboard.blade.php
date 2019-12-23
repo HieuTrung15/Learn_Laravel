55e9ea489d4e1a38c0ebb1cc9e21503dd50382c0
HieuTrung15 <hieutrungvietnam0107@gmail.com>
2019-12-23 22:20:21 +0700

    model done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,7 +17,7 @@
                     <ul class="navbar-nav mr-auto navbar-right">
                         <li><a href="/create">Create Post</a></li>
                     </ul>
-
+                    @if(count($posts) > 0)
                         <table class="table table-striped">
                             <tr>
                                 <td>Title</td>
@@ -26,12 +26,20 @@
                             </tr>
                             @foreach($posts as $post)
                                 <tr>
-                                    <th> {{$post->title}} </th>
-                                    <th><a href="posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a></th>
+                                    <td> {{$post->title}} </td>
+                                    <td><a href="posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a></td>
+                                    <td>
+                                        <form action=" {{url('destroy', $post->id)}} " method="post" class="pull-right">
+                                            @csrf
+                                            <button type="submit" class="btn btn-danger">Delete</button>
+                                        </form>
+                                    </td>
                                 </tr>
                             @endforeach
                         </table>
-
+                    @else
+                        <p>You don't have any post</p>
+                    @endif
                 </div>
             </div>
         </div>
